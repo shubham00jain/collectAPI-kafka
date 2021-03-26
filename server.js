4d1c28827fad16820a95e2bb02e0ab88119e9a6b
shubham656 <shubham656jain@gmail.com>
2021-03-26 21:58:22 +0530

    refactor

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const producer = require('./producer');
 const axios = require('axios');
 const CronJob = require('node-schedule');
-//const consumer = require('./consumer');
-
 
 const job = CronJob.scheduleJob('*/10 * * * * *', function() {
     producer.KafkaService.sendRecord('"mcGkTHWMPSoHGYXfzjWD"', "747e298d99dab12b80d0b81013a6542b", axios);
